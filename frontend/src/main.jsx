680d9c86a3a7a5058c680191bd568dab5c1d05ba
abhishek <bhshkvijayan@gmail.com>
2025-11-24 17:47:43 +0530

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
+
 createRoot(document.getElementById('root')).render(
   
-  <GoogleOAuthProvider clientId={clientId}>
   <StrictMode>
-    <App />
-  </StrictMode>,
-  </GoogleOAuthProvider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
 )
+
