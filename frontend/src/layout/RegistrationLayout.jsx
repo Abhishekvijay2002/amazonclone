f979a8b26f102fcbfc7a6bc3b4aa6e86d9e32f57
abhishek <bhshkvijayan@gmail.com>
2025-11-24 17:19:06 +0530

    first commit

--- frontend/src/layout/RegistrationLayout.jsx
+++ frontend/src/layout/RegistrationLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 
 function RegistrationLayout() {
